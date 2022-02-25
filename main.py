0a603b8477f58aa5dc4c32abfde5b25dc849dfa0
Ned-84 <nedalxzo@gmail.com>
2022-02-25 15:21:59 +0200

    create create_component function

--- main.py
+++ main.py
@@ -31,12 +31,42 @@ def clear():
 clear()
 
 
+WIN_WIDTH = 500
+WIN_HEIGHT = 600
+WIN_BG = "gray75"
+
+
 def start_app(root: tkinter.Tk):
     root.mainloop()
 
 
+
+
+def create_component(root: tkinter.Tk, component_name: str, img):
+    """this function will create the digital componet"""
+    img = tkinter.PhotoImage(
+        file=f"./assets/{component_name}.png", name=component_name)
+    component = tkinter.Label(root, bd=0, image=img, bg=WIN_BG)
+
+    return component
+
+
 def main_window():
-    pass
+
+    root = tkinter.Tk()
+
+    root.title("Logic Gates")
+
+    root.resizable(False, False)
+
+    root.geometry(f"{WIN_WIDTH}x{WIN_HEIGHT}")
+
+    root.configure(bg=WIN_BG)
+
+    and_gate = create_component(root, "and")
+    and_gate.pack()
+
+    start_app(root)
 
 
 def main():
